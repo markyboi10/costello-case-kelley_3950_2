de02f5fdf6fd6f3507fce7c78e9f11c13f0aea8e
Derek Costello <costellod@merrimack.edu>
2023-10-25 23:50:54 -0400

    Buttons added for sorting

--- HomePage/script.js
+++ HomePage/script.js
@@ -28,18 +28,63 @@ class Image {
     }
 }
 
+// Create an array of images
+const images = [
+    new Image('AMGGT.png', 'AMG GT Black Series', 'Road & Track'),
+    new Image('C7.png', 'C7 GT3-R', 'Registry of Corvette Race Cars'),
+    new Image('m4GT3.png', 'BMW M4 GT3', "Auto Addiction"),
+    new Image('z3.png', 'BMW Z3 GT3', 'BMW Blog'),
+    new Image('650s.png', 'McLaren 650s GT3', 'Sportscar Racing News'),
+    new Image('296Ferrari.png', "Ferrari 296 GT3", "Top Gear"),
+    new Image('PorscheVsSupra.png', "Porsche 911-R GT3 vs Toyota Supra GT3", "Daily Sport Car"),
+    new Image('R8LMS.png', "Audi R8 LMS GT3", "Road and Track"),
+    new Image('VantageGT3.png', "Aston Martin Vantage GT3", "Race Department"),
+    new Image('HuracanGT3.png', "Lamborghini Huracan GT3 EVO II", "Lamborghini"),
+];
+
+const albumDiv = document.getElementById('album');
+// Apply sortable to albumDiv
+new Sortable(albumDiv, {
+    animation: 400,
+    ghostClass: 'sortable-ghost',
+    onChoose: (evt) => {
+        canChangeHover = false;
+    },
+    onUnchoose: (evt) => {
+        canChangeHover = true;
+    }
+});
+
 // Sorting functions
 function sortRandomly(array){
     // Inspiration for randomization: https://www.geeksforgeeks.org/how-to-shuffle-an-array-using-javascript/#
     array.sort(() => Math.random() - 0.5);
+    updateListOnSort();
 }
 
 function sortBySource(array) {
     array.sort((image1, image2) => image1.citation.localeCompare(image2.citation));
+    updateListOnSort();
 }
 
 function sortByFileName(array) {
     array.sort((image1, image2) => image1.imgSrc.localeCompare(image2.imgSrc));
+    updateListOnSort();
+}
+
+function updateListOnSort() {
+    albumDiv.innerHTML = "";
+    images.forEach((image) => {
+        var imageCard = image.getFormattedHTML();
+        // Add events for hovering
+        imageCard.addEventListener("mouseover", hoverAdd, false);
+        imageCard.addEventListener("mouseout", hoverRemove, false);
+    
+        var addDiv = document.createElement('div');
+        addDiv.appendChild(imageCard);
+    
+        albumDiv.appendChild(addDiv);
+    });
 }
 
 // Hover events
@@ -54,43 +99,17 @@ hoverRemove = function (event) {
         event.target.classList.remove("hovered");
 }
 
-// Create an array of images
-const images = [
-    new Image('AMGGT.png', 'AMG GT Black Series', 'Road & Track'),
-    new Image('C7.png', 'C7 GT3-R', 'Registry of Corvette Race Cars'),
-    new Image('m4GT3.png', 'BMW M4 GT3', "Auto Addiction"),
-    new Image('z3.png', 'BMW Z3 GT3', 'BMW Blog'),
-    new Image('650s.png', 'McLaren 650s GT3', 'Sportscar Racing News'),
-    new Image('296Ferrari.png', "Ferrari 296 GT3", "Top Gear"),
-    new Image('PorscheVsSupra.png', "Porsche 911-R GT3 vs Toyota Supra GT3", "Daily Sport Car"),
-    new Image('R8LMS.png', "Audi R8 LMS GT3", "Road and Track"),
-    new Image('VantageGT3.png', "Aston Martin Vantage GT3", "Race Department"),
-    new Image('HuracanGT3.png', "Lamborghini Huracan GT3 EVO II", "Lamborghini"),
-];
+// Activate buttons
+var randomBtn = document.getElementById("randomBtn");
+var fileNameBtn = document.getElementById("fileNameBtn");
+var sourceBtn = document.getElementById("sourceBtn");
+
+randomBtn.addEventListener("click", () => sortRandomly(images), false);
+fileNameBtn.addEventListener("click", () => sortByFileName(images), false);
+sourceBtn.addEventListener("click", () => sortBySource(images), false);
+
+
 // Randomize on page startup
 sortRandomly(images);
 
-const albumDiv = document.getElementById('album');
-images.forEach((image) => {
-    var imageCard = image.getFormattedHTML();
-    // Add events for hovering
-    imageCard.addEventListener("mouseover", hoverAdd, false);
-    imageCard.addEventListener("mouseout", hoverRemove, false);
-
-    var addDiv = document.createElement('div');
-    addDiv.appendChild(imageCard);
-
-    albumDiv.appendChild(addDiv);
-});
-
-new Sortable(albumDiv, {
-    animation: 400,
-    ghostClass: 'sortable-ghost',
-    onChoose: (evt) => {
-        canChangeHover = false;
-    },
-    onUnchoose: (evt) => {
-        canChangeHover = true;
-    }
-});
 
